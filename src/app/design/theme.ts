9c1061dd8afcaefeffea568424ad66df228ffb75
Mark Carrington <frumarkcarrington@gmail.com>
2024-03-12 07:39:16 +0530

    added the components section set the assets set the components structure for the atomic design structure

---
+++ src/app/design/theme.ts
@@ -0,0 +1,35 @@
+const colors = {
+  lightMode: {
+    primary: "#CA9047",
+    onPrimary: "white",
+    secondary: "#431F17",
+    onSecondary: "white",
+    tertiary: "#515151",
+    onTertiary: "#white",
+    primaryContainer: "#EFDEC8",
+    onPrimaryContainer: "#140E07",
+    secondaryContainer: "#FBF7F6",
+    onSecondaryContainer: "#653930",
+    surfaceColors: {
+      surfaceLowest: "#FFFFFF",
+      surfaceLow: "#FAF4ED",
+      surfaceNormal: "#F4E9DA",
+      surfaceHigh: "#EAD3B5",
+      surfaceHighest: "#DFBC9",
+    },
+    prmaryVariants: {
+      primaryFixed: "#FAF4ED",
+      onPrimaryFixed: "#3D2B15",
+    },
+    secondaryVariants: {
+      secondaryFixed: "#FDFBFB",
+      onSecondaryFixed: "#8D6861",
+    },
+    tertiaryVariants: {
+      tertiaryFixed: "#EEEEEE",
+      onTertiaryFixed: "#424242",
+    },
+  },
+};
+
+export default colors;
