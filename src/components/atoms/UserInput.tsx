cec05e2641915b1635827ea71c3241c5ae54881b
Mark Carrington <frumarkcarrington@gmail.com>
2024-03-18 19:02:25 +0530

    built all the currently needed components

--- src/components/atoms/UserInput.tsx
+++ src/components/atoms/UserInput.tsx
@@ -1,7 +1,9 @@
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import "../../app/design/inputStyle.css";
 import { IoIosSearch } from "react-icons/io";
 import { IoMdClose } from "react-icons/io";
+import { MdOutlineStarRate } from "react-icons/md";
+import { MdStarRate } from "react-icons/md";
 
 interface inputFieldType {
   type: string;
@@ -15,6 +17,19 @@ type dropDownValues = {
   dropDownName: string;
   dropDownValues: string[];
 };
+
+interface rateInputType {
+  unRated: ReactNode;
+  rated: ReactNode;
+}
+
+const rateInputValues: rateInputType[] = [
+  { unRated: <MdOutlineStarRate />, rated: <MdStarRate /> },
+  { unRated: <MdOutlineStarRate />, rated: <MdStarRate /> },
+  { unRated: <MdOutlineStarRate />, rated: <MdStarRate /> },
+  { unRated: <MdOutlineStarRate />, rated: <MdStarRate /> },
+  { unRated: <MdOutlineStarRate />, rated: <MdStarRate /> },
+];
 
 function UserInput({
   type,
@@ -31,6 +46,25 @@ function UserInput({
       setSearchValue("");
     }
   }
+  const [inputComplete, setInputComplete] = useState({
+    first: true,
+    second: false,
+    third: false,
+    fourth: false,
+  });
+
+  const [cardNumberInput, setCardNumberInput] = useState({
+    first: "",
+    second: "",
+    third: "",
+    fourth: "",
+  });
+
+  function upDateInput(input: string, value: string) {
+    setCardNumberInput((prevInput) => {
+      return { ...prevInput, [input]: value };
+    });
+  }
 
   useEffect(() => {
     function checkIfUserSearch() {
@@ -41,7 +75,33 @@ function UserInput({
       }
     }
     checkIfUserSearch();
-  }, [searchValue]);
+    if (String(cardNumberInput.first).length === 4) {
+      setInputComplete((prev) => {
+        return {
+          ...prev,
+          first: false,
+          second: true,
+        };
+      });
+    } else if (String(cardNumberInput.second).length === 4) {
+      setInputComplete((prev) => {
+        return {
+          ...prev,
+          second: false,
+          third: true,
+        };
+      });
+    }
+    if (String(cardNumberInput.third).length === 4) {
+      setInputComplete((prev) => {
+        return {
+          ...prev,
+          third: false,
+          fourth: true,
+        };
+      });
+    }
+  }, [searchValue, cardNumberInput]);
 
   if (type === "simple") {
     return (
@@ -96,9 +156,63 @@ function UserInput({
       </div>
     );
   } else if (type === "rate") {
-    return <></>;
+    return (
+      <div className="rate-input">
+        <section className="input-section">
+          {rateInputValues.map(() => (
+            <span></span>
+          ))}
+        </section>
+      </div>
+    );
   } else if (type === "card") {
-    return <></>;
+    return (
+      <div className="card-input">
+        <section className="input-section">
+          <div className="top-section">
+            <input
+              type="number"
+              className="first"
+              placeholder="xxxx xxxx xxxx xxxx"
+              autoFocus={inputComplete.first}
+              value={cardNumberInput.first}
+              onChange={(e) => upDateInput("first", e.target.value)}
+            />
+            <input
+              type="number"
+              className="seconda"
+              autoFocus={inputComplete.second}
+              value={cardNumberInput.second}
+              onChange={(e) => upDateInput("second", e.target.value)}
+            />
+            <input
+              type="number"
+              className="third"
+              autoFocus={inputComplete.third}
+              value={cardNumberInput.third}
+              onChange={(e) => upDateInput("third", e.target.value)}
+            />
+            <input
+              type="number"
+              className="fourth"
+              autoFocus={inputComplete.fourth}
+              value={cardNumberInput.fourth}
+              onChange={(e) => upDateInput("fourth", e.target.value)}
+            />
+            <div className="account-icons"></div>
+          </div>
+          <div className="bottom-section">
+            <div className="expiry-date">
+              <input type="number" /> <p>/</p> <input type="number" />
+            </div>
+            <div className="cvc-code">
+              <input type="number" placeholder="CVC" />
+              <span className="cvc-icon"></span>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
   }
   return;
 }
