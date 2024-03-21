aa69ffa70f7e4ca37b9893fcb755687a170d6e3b
Mark Carrington <frumarkcarrington@gmail.com>
2024-03-22 00:07:01 +0530

    updated the links and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { Home, Login, Signup, TestPage } from "./components/pages";
+import {
+  Contact,
+  Home,
+  About,
+  Login,
+  Shopping,
+  Signup,
+  TestPage,
+} from "./components/pages";
 import { Header } from "./components/molecules";
 
 function App() {
@@ -10,6 +18,9 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/shopping" element={<Shopping />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/test" element={<TestPage />} />
--- src/components/data/index.ts
+++ src/components/data/index.ts
@@ -2,7 +2,7 @@ import { pageLinkTypes } from "../../types/vartypes";
 
 export const pageLinks: pageLinkTypes[] = [
   { name: "Home", to: "/home" },
-  { name: "Shopping", to: "/shop" },
+  { name: "Shopping", to: "/shopping" },
   { name: "About us", to: "/about" },
   { name: "Contact us", to: "/contact" },
 ];
