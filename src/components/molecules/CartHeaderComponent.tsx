aa1ec408401e2fbf78035dc6c48ed6781eb1eb5c
Mark Carrington <frumarkcarrington@gmail.com>
2024-03-23 17:14:06 +0530

    correct the styles on the cart component css

--- src/components/molecules/CartHeaderComponent.tsx
+++ src/components/molecules/CartHeaderComponent.tsx
@@ -1,5 +1,6 @@
 import { IoMdCart } from "react-icons/io";
 import { cartHeaderCompPropType } from "../../types/vartypes";
+import styled from "styled-components";
 
 function CartHeaderComponent({
   setOrderFunc,
@@ -7,7 +8,7 @@ function CartHeaderComponent({
   userNameInitial,
 }: cartHeaderCompPropType) {
   return (
-    <div>
+    <CartContainer>
       <div className="mob-user-btns">
         <div className="cart-btn" onClick={setOrderFunc}>
           <span className="cart-icon">
@@ -21,7 +22,77 @@ function CartHeaderComponent({
           </span>
         </button>
       </div>
-    </div>
+    </CartContainer>
   );
 }
 export default CartHeaderComponent;
+
+const CartContainer = styled.header`
+  width: fit-content;
+  justify-self: flex-end;
+  margin-left: 15%;
+  .mob-user-btns {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 20px;
+
+    .cart-btn {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+      border-radius: 50%;
+      background-color: rgba(251, 247, 246, 1);
+      border: none;
+      &:hover {
+        background-color: rgba(234, 215, 211, 1);
+      }
+      cursor: pointer;
+      .cart-icon {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 12px;
+      }
+      .order-number {
+        position: absolute;
+        top: -10px;
+        right: 0;
+        background-color: rgba(67, 31, 23, 1);
+        color: white;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 3px;
+        border-radius: 20px;
+        h1 {
+          font-size: 12px;
+          font-family: "El Messiri";
+        }
+      }
+    }
+
+    .account-btn {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border: none;
+      padding: 10px 12px;
+      border-radius: 50%;
+      background-color: rgba(67, 31, 23, 1);
+      color: rgba(234, 215, 211, 1);
+      cursor: pointer;
+      span {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 3px 2px;
+        h1 {
+          font-size: 16px;
+        }
+      }
+    }
+  }
+`;
