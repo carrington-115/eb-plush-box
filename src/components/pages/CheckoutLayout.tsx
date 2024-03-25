ff0c2a383b106eef84aee23cd0f549c52a526269
Mark Carrington <frumarkcarrington@gmail.com>
2024-03-25 14:05:08 +0530

    built the basic structure of the layout.css file and added it to the CheckoutLayout.tsx

--- src/components/pages/CheckoutLayout.tsx
+++ src/components/pages/CheckoutLayout.tsx
@@ -1,4 +1,16 @@
+import { Outlet } from "react-router-dom";
+import { Accordion, Newsletter } from "../organisms";
+import "../../app/design/layout.css";
+
 function CheckoutLayout() {
-  return <div>CheckoutLayout</div>;
+  return (
+    <div className="page-container">
+      <div className="page-details-container">
+        <Outlet />
+      </div>
+      <Accordion />
+      <Newsletter />
+    </div>
+  );
 }
 export default CheckoutLayout;
