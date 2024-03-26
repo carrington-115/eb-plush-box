3b0700dfbe414460d7d9ec95579e24b2c16886ca
Mark Carrington <frumarkcarrington@gmail.com>
2024-03-26 07:02:26 +0530

    built the style for the step component

--- src/components/atoms/StepComponent.tsx
+++ src/components/atoms/StepComponent.tsx
@@ -4,11 +4,29 @@ import { stepCompPropsType } from "../../types/vartypes";
 function StepComponent({ number, title }: stepCompPropsType) {
   return (
     <Container>
-      <span>{number}</span>
+      <span>
+        <p>{number}</p>
+      </span>
       <p>{title}</p>
     </Container>
   );
 }
 export default StepComponent;
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 20px;
+  gap: 20px;
+  width: 100%;
+  span {
+    padding: 5px 12px;
+    border-radius: 20px;
+    background-color: rgba(101, 57, 48, 1);
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 20px;
+  }
+`;
--- src/components/pages/Cart.tsx
+++ src/components/pages/Cart.tsx
@@ -1,4 +1,46 @@
+import styled from "styled-components";
+import { StepComponent } from "../atoms";
+
+const steps: string[] = [
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+];
+
 function Cart() {
-  return <div className="page-container">Cart</div>;
+  return (
+    <>
+      <Steps>
+        <h2>Complete your order</h2>
+        {steps.map((text, index) => (
+          <StepComponent key={index} number={index + 1} title={text} />
+        ))}
+      </Steps>
+      <FormSection></FormSection>
+    </>
+  );
 }
 export default Cart;
+
+const Steps = styled.section`
+  display: flex;
+  flex-direction: column;
+  padding-bottom: 50px;
+  width: 40%;
+  gap: 20px;
+
+  @media (min-width: 320px) and (max-width: 599px) {
+    margin-bottom: 50px;
+    display: flex;
+    gap: 15px;
+    width: 100%;
+  }
+
+  h2 {
+    align-self: center;
+    font-size: 24px;
+  }
+`;
+
+const FormSection = styled.section``;
