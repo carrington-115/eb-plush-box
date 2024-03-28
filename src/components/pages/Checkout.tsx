a3352c46682b93503a2c4e1e4990f64c42e0c29a
Mark Carrington <frumarkcarrington@gmail.com>
2024-03-28 11:47:13 +0530

    fixed checkout page content

--- src/components/pages/Checkout.tsx
+++ src/components/pages/Checkout.tsx
@@ -8,7 +8,7 @@ function Checkout() {
       <Steps
         steps={[
           "Thank you for choosing EB Plush for your shopping needs! We appreciate your support and trust in us. Happy shopping, and we hope you enjoy your purchases! If you have any questions or need assistance, feel free to reach out to our customer support team. Thank you again for shopping with us!",
-          "Proceed by filling the form to rate us about your experience",
+          "Proceed by filling the form to complete your order",
         ]}
       />
       <FormSection>
